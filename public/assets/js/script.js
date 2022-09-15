4b2aad6851433cc3220542acd2ca6f699cbf3ac9
Jhomax Torres Medina <73907885+Jhomax21@users.noreply.github.com>
2022-09-14 21:18:53 -0500

    Add files via upload

---
+++ public/assets/js/script.js
@@ -0,0 +1,8 @@
+function sendEmail() {
+    alert("¡Correo enviado!");
+    top.location.href = "about.html";
+}
+function msgSubscription() {
+    alert("¡Gracias por suscribirte!");
+    top.location.href = "plans.html";
+}
